e4e09ab7b048c3d2c3fe040417614c49b9bf9891
MarkusZehnle <60010315+MarkusZehnle@users.noreply.github.com>
2024-04-22 16:48:13 +0200

    Create InstallAppsViaWinget.ps1

---
+++ InstallAppsViaWinget.ps1
@@ -0,0 +1,9 @@
+#Write-Output "Install Apps..."
+
+# Install Adobe Reader DC
+winget install "Adobe Acrobat Reader DC" --source msstore --silent --accept-package-agreements --accept-source-agreements
+
+# Install VLC
+winget install VLC --source msstore --silent --accept-package-agreements --accept-source-agreements
+
+#Write-Output "Bla"
